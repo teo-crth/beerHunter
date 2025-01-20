0c75fd9f2748bf23e2f3a9f3e6dcaf7ff7b27d47
teo-crth <teoconrath@gmail.com>
2025-01-20 16:58:24 +0100

    routes in router

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,51 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+const barControllers = require("./controllers/barControllers");
+const beerControllers = require("./controllers/beerControllers");
+const usersControllers = require("./controllers/usersControllers");
+const userCommentControllers = require("./controllers/userCommentControllers");
+const beerAvailableControllers = require("./controllers/beerAvailableControllers");
+const commentImageControllers = require("./controllers/commentImageControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// GET
+router.get("/bars/:id/beers", beerAvailableControllers.browse);
+router.get("/bars/:id/comments", barControllers.browse);
+router.get("/bars/:id", barControllers.read);
+router.get("/bars/:city", barControllers.browse);
+router.get("/bars", barControllers.browse);
+
+router.get("/beers/:id", beerControllers.read);
+router.get("/beers/:type", beerControllers.browse);
+router.get("/beers", beerControllers.browse);
+
+router.get("/users/:id/comments/", usersControllers.browse);
+router.get("/users/:id/favorite-bars/", usersControllers.browse);
+router.get("/users/:id", usersControllers.read);
+router.get("/users", usersControllers.browse);
+
+router.get("/comments/:id/images", commentImageControllers.browse);
+router.get("/comments/:id", userCommentControllers.read);
+router.get("/comments", userCommentControllers.browse);
+
+
+// PUT
+router.put("/bars/:id", barControllers.edit);
+router.put("/beers/:id", beerControllers.edit);
+router.put("/users/:id", usersControllers.edit);
+router.put("/comments/:id", userCommentControllers.edit);
+
+
+// POST
+router.post("/bars", barControllers.add);
+router.post("/beers", beerControllers.add);
+router.post("/users", usersControllers.add);
+router.post("/comments", userCommentControllers.add);
+
+// DELETE
+router.delete("/bars/:id", barControllers.destroy);
+router.delete("/beers/:id", beerControllers.destroy);
+router.delete("/users/:id", usersControllers.destroy);
+router.delete("/comments/:id", userCommentControllers.destroy);
 
 module.exports = router;
