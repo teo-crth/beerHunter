6a3a2da5fe1a50eeb7e559c0e87f10849ab749bf
Paul-LOMBARD <paul.lombard26600@gmail.com>
2025-01-20 15:55:24 +0100

    crud user comment

---
+++ backend/src/models/favorite_barManager.js
@@ -0,0 +1,21 @@
+const AbstractManager = require("./AbstractManager");
+
+class favorite_barManager extends AbstractManager {
+    constructor(){
+        super({table: "favorite_bar"});
+    }
+
+    insert(favorite_bar){
+        return this.database.query(
+            `INSERT INTO ${this.table} (user_id, bar_id) VALUES ($1, 2$)`,
+            [favorite_bar.user_id, favorite_bar.bar_id]
+        )
+    }
+
+    update(favorite_bar) {
+        return this.database.query(
+          `UPDATE ${this.table} SET user_id= $1, bar_id= $2 WHERE id = $3`,
+          [favorite_bar.user_id, favorite_bar.bar_id, favorite_bar.id]
+        );
+      }
+}
