726fce70bd19448e01b5a02c4ebae18d8452ffdc
teo-crth <teoconrath@gmail.com>
2025-01-21 09:14:29 +0100

    api route

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,43 +10,43 @@ const beerAvailableControllers = require("./controllers/beerAvailableControllers
 const commentImageControllers = require("./controllers/commentImageControllers");
 
 // GET
-router.get("/bars/:id/beers", beerAvailableControllers.browse);
-router.get("/bars/:id/comments", barControllers.browse);
-router.get("/bars/:id", barControllers.read);
-router.get("/bars/:city", barControllers.browse);
-router.get("/bars", barControllers.browse);
+router.get("api/bars/:id/beers", beerAvailableControllers.browse);
+router.get("api/bars/:id/comments", barControllers.browse);
+router.get("api/bars/:id", barControllers.read);
+router.get("api/bars/:city", barControllers.browse);
+router.get("api/bars", barControllers.browse);
 
-router.get("/beers/:id", beerControllers.read);
-router.get("/beers/:type", beerControllers.browse);
-router.get("/beers", beerControllers.browse);
+router.get("api/beers/:id", beerControllers.read);
+router.get("api/beers/:type", beerControllers.browse);
+router.get("api/beers", beerControllers.browse);
 
-router.get("/users/:id/comments/", usersControllers.browse);
-router.get("/users/:id/favorite-bars/", usersControllers.browse);
-router.get("/users/:id", usersControllers.read);
-router.get("/users", usersControllers.browse);
+router.get("api/users/:id/comments/", usersControllers.browse);
+router.get("api/users/:id/favorite-bars/", usersControllers.browse);
+router.get("api/users/:id", usersControllers.read);
+router.get("api/users", usersControllers.browse);
 
-router.get("/comments/:id/images", commentImageControllers.browse);
-router.get("/comments/:id", userCommentControllers.read);
-router.get("/comments", userCommentControllers.browse);
+router.get("api/comments/:id/images", commentImageControllers.browse);
+router.get("api/comments/:id", userCommentControllers.read);
+router.get("api/comments", userCommentControllers.browse);
 
 
 // PUT
-router.put("/bars/:id", barControllers.edit);
-router.put("/beers/:id", beerControllers.edit);
-router.put("/users/:id", usersControllers.edit);
-router.put("/comments/:id", userCommentControllers.edit);
+router.put("api/bars/:id", barControllers.edit);
+router.put("api/beers/:id", beerControllers.edit);
+router.put("api/users/:id", usersControllers.edit);
+router.put("api/comments/:id", userCommentControllers.edit);
 
 
 // POST
-router.post("/bars", barControllers.add);
-router.post("/beers", beerControllers.add);
-router.post("/users", usersControllers.add);
-router.post("/comments", userCommentControllers.add);
+router.post("api/bars", barControllers.add);
+router.post("api/beers", beerControllers.add);
+router.post("api/users", usersControllers.add);
+router.post("api/comments", userCommentControllers.add);
 
 // DELETE
-router.delete("/bars/:id", barControllers.destroy);
-router.delete("/beers/:id", beerControllers.destroy);
-router.delete("/users/:id", usersControllers.destroy);
-router.delete("/comments/:id", userCommentControllers.destroy);
+router.delete("api/bars/:id", barControllers.destroy);
+router.delete("api/beers/:id", beerControllers.destroy);
+router.delete("api/users/:id", usersControllers.destroy);
+router.delete("api/comments/:id", userCommentControllers.destroy);
 
 module.exports = router;
