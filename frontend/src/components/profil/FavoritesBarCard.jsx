8452afb7b77f2c042343f4edd75901f93e8f1ac0
teo-crth <teoconrath@gmail.com>
2025-01-30 13:36:05 +0100

    axios comments

--- frontend/src/components/profil/FavoritesBarCard.jsx
+++ frontend/src/components/profil/FavoritesBarCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FavoritesBarCard = () => {
     return (
-        <div className='flex flex-col w-full'>
+        <div className='flex flex-col w-full justify-center items-center'>
             <h3 className='font-title font-bold text-light light-mode:text-dark-black text-center lg:text-left'>Vos bars favoris</h3>
         </div>
     );
