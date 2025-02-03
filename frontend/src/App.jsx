0b01f93cab39a2445629fe5810e978c952e7c1c0
Paul-LOMBARD <paul.lombard26600@gmail.com>
2025-02-03 13:38:47 +0100

    page beer et bar 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import Home from "./pages/Home";
-import BeerPage from "./pages/beerPage/BeerPage";
-import BarPage from "./pages/barPage/BarPage";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BeerPage />
+      <Home />
 
     </div>
   );
