58d567fd3ccc05ac62a29ad1e2d4c562e70d7215
teo-crth <teoconrath@gmail.com>
2025-02-04 11:42:08 +0100

    searchbar

--- frontend/src/components/ui/SearchBar.jsx
+++ frontend/src/components/ui/SearchBar.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchAllCities } from '../../api/city/cityCrud';
+import Button from './Button';
 
 const SearchBar = () => {
+    const [ cities, setCities ] = useState([]);
+    const [ searchTerms, setSearchTerms ] = useState('');
+
+    useEffect(() => {
+        const fetchCitiesData = async () => {
+
+            try {
+                const response = await fetchAllCities();
+                setCities(response);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des villes:', error);
+            }
+        }
+
+        fetchCitiesData();
+
+    }, []);
+
+    const handleChange = (e) => {
+        setSearchTerms(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Recherche de ville');
+    }
+
     return (
-        <div className='w-full flex items-center justify-center'>
-            
+        <div className='w-full md:w-2/3 lg:w-1/2 flex items-center justify-center'>
+            <form action="submit" className='w-full flex items-center justify-between text-left border-primary border-2 rounded-lg m-5 text-light light-mode:text-dark-black'>
+                <input type="text" list="cities" className='w-2/3 p-2 bg-transparent border-0' placeholder='Entrez une ville' onChange={handleChange} value={searchTerms} />
+                <Button type='submit' onclick={handleSubmit} className='bg-primary text-light light-mode:bg-dark rounded-r-lg h-10' text="Rechercher" />
+            </form>
             
         </div>
     );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import SearchBar from "../components/ui/SearchBar";
 
 export default function Home() {
   return (
-   <div className="container-home flex flex-col items-center justify-center bg-dark light-mode:bg-light pb-5  text-light light-mode:text-dark-black">
+   <div className="container-home flex flex-col items-center justify-start bg-dark light-mode:bg-light pb-5 min-h-dvh text-light light-mode:text-dark-black">
     <section className="presentation font-text text-center flex flex-col items-center justify-center pt-5">
       <h1 className="text-2xl font-bold p-1">Bienvenue sur BeerHunter !</h1>
       <h2 className="text-xl">Ne restez jamais sur votre soif</h2>
