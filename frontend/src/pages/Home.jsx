e1ce104cc54126829c6d93b9f91da34051233ea3
teo-crth <teoconrath@gmail.com>
2025-02-06 15:36:06 +0100

    background home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,13 +4,13 @@ import SearchBar from "../components/ui/SearchBar";
 export default function Home() {
 
     return (
-        <>
+        <div className="container-homePage min-h-full">
             <section className="presentation font-text text-center flex flex-col items-center justify-center text-light light-mode:text-dark-black pt-5">
                 <h1 className="text-2xl font-bold ">Bienvenue sur BeerHunter !</h1>
                 <h2 className="text-xl">Ne restez jamais sur votre soif</h2>
-                <h3 className="text-xs mt-2">Trouver les bars près de chez vous qui servent votre bière préférée</h3>
+                <h3 className="text-xs m-2">Trouver les bars près de chez vous qui servent votre bière préférée</h3>
             </section>
             <SearchBar />
-        </>
+        </ div>
     );
 }
