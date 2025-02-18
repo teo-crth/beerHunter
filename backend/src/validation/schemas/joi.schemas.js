2ad13d3d7b57a677dffa323d5a543d149f9c3b38
teo-crth <teoconrath@gmail.com>
2025-02-18 11:41:17 +0100

    validation joi

--- backend/src/validation/schemas/joi.schemas.js
+++ backend/src/validation/schemas/joi.schemas.js
@@ -22,7 +22,7 @@ const createUserSchema = Joi.object({
         .max(30)
         .pattern(/(?=.*[A-Z])/, 'at least one uppercase letter')
         .pattern(/(?=.*\d)/, 'at least one digit')
-        .pattern(/(?=.*[!@#$%^&*(),.?":{}|<>])/ , 'at least one special character')
+        .pattern(/(?=.*[!@#$%^&*(),.?":{}|<>-_])/ , 'at least one special character')
         .required(),
     confirmPassword: Joi.ref('password'),
     profil_picture: Joi.string().allow(null),
