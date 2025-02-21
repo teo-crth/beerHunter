e7e8d49e7f99b2660147df8d87a69b07451f5211
teo-crth <teoconrath@gmail.com>
2025-02-21 10:49:55 +0100

    css beerPage

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
     }
 
     return (
-        <div className='container-contact flex flex-col items-center justify-start w-full light-mode:bg-light p-5'>
-            <h1 className='font-title font-bold text-2xl mb-5 text-light light-mode:text-dark-black'>Nous contacter</h1>
+        <div className='container-contact min-h-[calc(100vh-159px)] flex flex-col items-center justify-start w-full light-mode:bg-light p-5'>
+            <h1 className='font-title font-bold text-2xl mb-5 text-light light-mode:text-dark-black'>NOUS CONTACTER</h1>
             <form onSubmit={handleSubmit} className='container-form w-full md:w-1/2 lg:w-1/3 min-h-100 flex flex-col items-start justify-between text-center gap-0.5 shadow-md bg-dark-black light-mode:bg-light text-light light-mode:text-dark rounded-lg p-5'>
                 <h3 className='font-text font-bold text-xl text-center w-full'>Formulaire de contact</h3>
                 
--- frontend/src/pages/barPage/BarPage.jsx
+++ frontend/src/pages/barPage/BarPage.jsx
@@ -106,98 +106,75 @@ const BarPage = () => {
     }
 
     return (
-        <div style={{ fontFamily: 'Arial, sans-serif', margin: '20px', padding: '20px', border: '1px solid #ddd', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '6em' }}>
-                <img
-                    src={`${BASE_URL}${bar.bar_picture}`}
-                    alt={`photo ${bar.name}`}
-                    style={{ width: '50em', height: '30em', borderRadius: '1em', marginRight: '4em' }}
-                />
-                <div>
-                    <h1 style={{ margin: '0 0 10px', color: '#333' }}>{bar.name}</h1>
-                    <div style={{ marginTop: '10px', display: 'flex', alignItems: 'center' }}>
-                        <span style={{ color: '#333', fontWeight: 'bold', marginRight: '10px' }}>
-                            Note : {bar.rate}/5
-                        </span>
-                        <ReactStars
-                            count={5}
-                            value={bar.rate}
-                            size={24}
-                            activeColor="#FEC514"
-                            edit={false}
-                        />
-                        {isLogin && !user?.favoritesBars?.some(favBar => favBar.id === bar.id) && (
-                            <Button text="Ajouter aux favoris" className="bg-primary" onClick={handleFavoriteClick}/>
-                        )}
-                        {isLogin && user?.favoritesBars?.some(favBar => favBar.id === bar.id) && (
-                            <Button text="Supprimer des favoris" className="bg-primary" onClick={handleDeleteFavoriteClick}/>
-                        )}
-                    </div>
+        <div className='container-barPage min-h-[calc(100vh-159px)] flex flex-col items-center justify-center w-full light-mode:bg-light md:p-10 lg:p-20 xl:p-20'>
+            <section className="container-bar-img-description flex flex-wrap justify-center items-center w-full mb-5">
+                <div className="container-img flex flex-col justify-center items-center w-[95%] md:w-[40%] lg:w-[40%] xl:w-[40%] h-[25em]">
+                    <img
+                        src={`${BASE_URL}${bar.bar_picture}`}
+                        alt={`photo ${bar.name}`}
+                        className='rounded-lg object-cover h-full w-full shadow-md'
+                    />
+                    {isLogin && !user?.favoritesBars?.some(favBar => favBar.id === bar.id) && (
+                        <Button text="Ajouter aux favoris" className="bg-primary" onClick={handleFavoriteClick}/>
+                    )}
+                    {isLogin && user?.favoritesBars?.some(favBar => favBar.id === bar.id) && (
+                        <Button text="Supprimer des favoris" className="bg-primary" onClick={handleDeleteFavoriteClick}/>
+                    )}
                 </div>
-            </div>
-
-            <div style={{ display: 'flex', width: '100%', marginTop: '2em' }}>
-                <div style={{ width: '40%', textAlign: 'center' }}>
-                    <h1 className='font-title text-center text-md text-light light-mode:text-dark-black font-bold p-1'>Bières disponibles</h1>
-                    <div className='w-full items-center'>
-                        <Slider {...settings}>
-                            {beersAvailable.length > 0 && beersAvailable.map((beer, index) => (
-                                <div key={index} className='flex items-center justify-center p-1 cursor-pointer' onClick={() => handleBeerClick(beer.id)} aria-label={`Navigation vers la page de la bière ${beer.name}`}>
-                                    <img src={`${BASE_URL}${beer.image_link}`} alt={`Biere-${index}`} className='border-1 border-primary rounded-2xl items-center' />
-                                </div>
+                <div className='container-description flex flex-col justify-start gap-3 p-3 items-start w-[95%] md:w-[60%] lg:w-[60%] xl:w-[60%] h-[25em]'>
+                    <div className="container-title w-full flex flex-col justify-start items-center mt-2">
+                        <h1 className='font-title font-bold text-center text-2xl text-light light-mode:text-dark-black tracking-wider'>{bar.name}</h1>
+                        <div className='container-rating flex items-center justify-center gap-2'>
+                            <span className='font-bold font-text text-md text-light light-mode:text-dark-black'>
+                                Note : {bar.rate}/5
+                            </span>
+                            <ReactStars
+                                count={5}
+                                value={bar.rate}
+                                size={24}
+                                activeColor="#FEC514"
+                                edit={false}
+                            />
+                        </div>
+                    </div>
+                    <div className='container-openingHours w-full flex  flex-col justify-center items-center gap-3'>
+                        <h1 className='font-bold font-title text-light light-mode:text-dark-black text-md text-center'>Horaires d'ouverture</h1>
+                        <ul className='font-text text-light light-mode:text-dark-black text-md text-center flex flex-wrap justify-center items-center gap-1'>
+                            {openingHours.map((item, index) => (
+                                <li key={index} className='border-2 border-primary rounded-lg p-0.5 whitespace-nowrap'>
+                                    <strong>{item.day} :</strong> {item.hours}
+                                </li>
                             ))}
-                        </Slider>
+                        </ul>
+                    </div>
+                    <div className='container-availableBeers flex flex-col justify-start items-center w-full'>
+                        <h1 className='font-title text-center text-lg text-light light-mode:text-dark-black font-bold'>Bières disponibles pour ce bar</h1>
+                        <div className='w-full md:w-[80%] lg:w-[80%] xl:w-[80%] items-center'>
+                            <Slider {...settings}>
+                                {beersAvailable.length > 0 && beersAvailable.map((beer, index) => (
+                                    <div key={index} className='flex items-center justify-center p-1 cursor-pointer' onClick={() => handleBeerClick(beer.id)} aria-label={`Navigation vers la page de la bière ${beer.name}`}>
+                                        <img src={`${BASE_URL}${beer.image_link}`} alt={`Biere-${index}`} className='border-1 border-primary rounded-2xl items-center' />
+                                    </div>
+                                ))}
+                            </Slider>
+                        </div>
                     </div>
                 </div>
-
-                <div style={{ width: '30%', marginLeft: 'auto' }}>
+            </section>
+            <aside className="flex w-full mt-5 flex-col items-center justify-center">
+                <div className='container-map flex flex-col items-center justify-center w-[90%] md:w-[60%] lg:w-[60%] xl:w-[60%]'>
                     <LoadScript googleMapsApiKey={GOOGLE_KEY} onLoad={handleScriptLoad} libraries={LIBRAIRIES}>
-                        <div ref={mapRef} style={{ width: '100%', height: '300px' }} />
+                        <div ref={mapRef} className='shadow-md rounded-lg w-full h-80' />
                     </LoadScript>
-                    <div style={{ marginTop: '1em', color: '#333', textAlign: 'center' }}>
+                    <div className='container-address flex flex-col items-center justify-center w-full text-light light-mode:text-dark-black p-2'>
                         <p className='font-text text-sm'><strong>Adresse : </strong>{bar.address}</p>
                         <p className='font-text text-sm'><strong>Téléphone : </strong>{bar.phone_number}</p>
-                        <a href={bar.website} target='blank' className='font-text text-sm text-blue-600 underline cursor-pointer'>Site internet</a>
+                        <a href={bar.website} target='blank' className='font-text text-md text-blue-600 underline cursor-pointer'>Site internet</a>
                     </div>
                 </div>
-            </div>
-
-            <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between', alignItems: 'center', marginTop: '2em' }}>
-                <div style={{ width: '40%' }}>
-                    <h1 style={{ textAlign: 'center', color: '#333', marginBottom: '1em' }}>Horaires d'ouverture</h1>
-                    <ul style={{ listStyleType: 'none', padding: 0, textAlign: 'center', color: '#666' }}>
-                        {openingHours.map((item, index) => (
-                            <li key={index} style={{ marginBottom: '5px' }}>
-                                <strong>{item.day} :</strong> {item.hours}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div style={{ width: '60%', textAlign: 'center', display: 'flex', justifyContent: 'center', gap: '10px', marginRight: 'auto' }}>
-                    <button style={{
-                        padding: '10px 20px',
-                        backgroundColor: '#FFA500',
-                        color: '#fff',
-                        border: 'none',
-                        borderRadius: '25px',
-                        cursor: 'pointer',
-                        fontWeight: 'bold'
-                    }}>
-                        Ajoutez Bière
-                    </button>
-                    <button style={{
-                        padding: '10px 20px',
-                        backgroundColor: '#FFA500',
-                        color: '#fff',
-                        border: 'none',
-                        borderRadius: '25px',
-                        cursor: 'pointer',
-                        fontWeight: 'bold'
-                    }}>
-                        Ajoutez Commentaire
-                    </button>
-                </div>
+            </aside>
+            <div className='coontainer-button flex flex-col items-center justify-center w-full p-5'>
+                <Button text="Ajouter un commentaire" className='bg-primary hover:bg-secondary' />
             </div>
             <Modal />
         </div>
