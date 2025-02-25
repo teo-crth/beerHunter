d531b00b005618cf77b712b0424ec7b1550ee6cc
teo-crth <teoconrath@gmail.com>
2025-02-25 17:10:53 +0100

    change vercel.json

--- frontend/src/components/ui/Modal.jsx
+++ frontend/src/components/ui/Modal.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../context/context';
 
-import EditForm from '../modals/EditForm';
-import EditPassword from '../modals/EditPassword';
-import DeleteProfil from '../modals/DeleteProfil';
-import DeleteComment from '../modals/DeleteComment';
-import Connexion from '../modals/Connexion';
-import Signup from '../modals/Signup';
-import MessageModal from '../modals/MessageModal';
-import AddComment from '../modals/AddComment';
+import EditForm from '../Modals/EditForm';
+import EditPassword from '../Modals/EditPassword';
+import DeleteProfil from '../Modals/DeleteProfil';
+import DeleteComment from '../Modals/DeleteComment';
+import Connexion from '../Modals/Connexion';
+import Signup from '../Modals/SignUp';
+import MessageModal from '../Modals/MessageModal';
+import AddComment from '../Modals/AddComment';
 
 const Modal = () => {
   const { closeModal, modalState } = useContext(AppContext);
