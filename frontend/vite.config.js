684c0a54eda59684b679c94bde53994864b523eb
teo-crth <teoconrath@gmail.com>
2025-02-25 17:42:57 +0100

    change vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()], 
+  base: '/frontend/',
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
