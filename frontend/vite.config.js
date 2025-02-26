321a229cb4165316c34f7b0936764a3f18c9b313
teo-crth <teoconrath@gmail.com>
2025-02-26 09:48:07 +0100

    change config vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()], 
-  base: "./",
+  base: "/",
   server: {
     historyApiFallback: true,
+  },
+  build: {
+    outDir: "dist",
   },
   resolve: {
     alias: {
